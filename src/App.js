27010b30d81c8b7af6e8643a1ebc245adbcb186b
Nick Chinsen <nicholaschinsen@gmail.com>
2020-12-03 00:42:22 -0500

    connected api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import WeatherList from './WeatherList';
+import API from './API';
 
-function App() {
-  return (
-    <WeatherList/>
-  );
+class App extends React.Component{
+  state={
+    forecast: [],
+    loading: true
+  }
+  //load data from api using my api key
+  async componentDidMount() {
+    try {
+        let forecast = await API.get('http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=3ec447546b00e21f3d79c5f5167d121a');
+        this.setState({forecast: forecast, loading: false})
+    } catch(e) {
+        console.log(`Axios request failed: ${e}`)
+    }        
+  }
+  render(){ 
+    if(this.state.loading){//waits for data to load so no undefined errors
+      return <div>loading...</div>
+    }
+    return(
+    <div className='Container'>
+    <h1>Local Forecast</h1>
+    <p>{this.state.forecast.data.name+', '+this.state.forecast.data.sys.country}</p>
+    </div>
+  );}
 }
 
 export default App;
--- src/WeatherList.js
+++ src/WeatherList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import API from './API';
 
 export default class WeatherList extends Component {
@@ -7,20 +6,16 @@ export default class WeatherList extends Component {
         forecast: [],
     }
 
-    async componentDidMount() {
-        try {
-            let forecast = await API.get('http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=3ec447546b00e21f3d79c5f5167d121a');
-            this.setState({forecast})
-        } catch(e) {
-            console.log(`Axios request failed: ${e}`)
-        }        
-    }
+
 
     render() {
         return(
             <div className='Container'>
                 <h1>Local Forecast</h1>
+                <p>{this.state.forecast.data.name}</p>
             </div>
         )
     }
+
+
 }
